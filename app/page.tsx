36db6ead51742abc568cb1bd876522f66e4be8ad
Georgiana Barefield <110576775+gbaref949@users.noreply.github.com>
2024-04-03 10:09:37 -0700

    worked on the profile

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default async function Home() {
   return (
-    <section>
+    <>
       <h1>Welcome, click to <strong><Link href={'/login'}>Login</Link></strong></h1>
-    </section>
+    </>
   );
 }
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,12 +1,24 @@
-import {getSession} from '../lib'
+import { redirect } from "next/navigation";
+import {getSession, logout} from '../lib'
 
 export default async function Profile() {
   const session = await getSession();
+  let user = session?.user
+//   console.log(session)
+  console.log(user)
   return (
     <section>
-      {/* <h1>Welcome{user}</h1> */}
-      <h1>Hello ___</h1>
+      <h1>Hello User</h1>
+      <form action={async (formdata)=>{
+        'use server'
+        await logout()
+        redirect('/')
+      }}>
+        
+        <button type="submit">Logout</button>
+      </form>
       <pre>{JSON.stringify(session,null,2)}</pre>
+      <pre>{user.name `1`}</pre>
     </section>
-  );
+  )
 }
